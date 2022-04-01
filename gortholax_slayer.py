8b2634f66c74d23bde8c501d75101d2cb581bd99
ZachM11 <102777104+ZachM11@users.noreply.github.com>
2022-03-31 19:22:16 -0700

    Add files via upload

---
+++ gortholax_slayer.py
@@ -0,0 +1,9 @@
+from os import system
+
+pName = input("ENTER NAME HERE\n")
+print("\nYou get punched in the face.\n")
+input("Press Enter to continue.")
+system('cls')
+print("Get it together, Sergeant! We got a Space Wyrm to kill! Now, get the lead out of ur pants and man the guns!\n")
+input("Press Enter to continue.")
+system('cls')
